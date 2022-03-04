fc90840a5a76282d736c7ce753ce7801f9b12d09
GastyDev <91214872+GastDevelopment@users.noreply.github.com>
2022-03-04 15:40:00 +0000

    Final Patch
    Last update
    - Cleaned code
    - Added lowercase support
    - Fixed bugs

--- Application.py
+++ Application.py
@@ -11,14 +11,14 @@ def checkMarks(m):
         pass
 
 def checkPupils(p):
-    if p > 60 or p < 20:
+    if p > 60 or p < 5:
         print("Too little or too many pupils! Enter from 1-60!\nSystem shutting down...")
         sys.exit()
     else:
         pass
 
 def checkDifficulty(d):
-    if d == "EASY" or d == "MEDIUM" or d == "HARD":
+    if d == "EASY" or d == "MEDIUM" or d == "HARD" or d == "easy" or d == "medium" or d == "hard" :
         pass
     else:
         print("Unkown difficulty, enter either EASY, MEDIUM or HARD\nSystem shutting down...")
@@ -28,38 +28,38 @@ global results
 results = []
 def testRandomizer(r, m, p):
     global results
-    if r == "EASY":
+    if r == "EASY" or r == "easy":
         for i in range(p):
             if m in range(20, 41):
-                e1 = max(randint(5, m), randint(5, (m + 1)))
+                e1 = max(randint(5, m), randint(5, m))
                 results.append(e1)
             elif m in range(41, 61):
-                e2 = max(randint(20, m), randint(20, (m + 1)))
+                e2 = max(randint(20, m), randint(20, m))
                 results.append(e2)
             elif m in range(60, 100):
-                e3 = max(randint(28, m), randint(28, (m + 1)))
+                e3 = max(randint(28, m), randint(28, m))
                 results.append(e3)
-    elif r == "MEDIUM":
+    elif r == "MEDIUM" or r == "medium":
         for i in range(p):
             if m in range(20, 41):
-                s1 = max(randint(3, m), randint(3, (m + 1)))
+                s1 = max(randint(3, m), randint(3, m))
                 results.append(s1)
             elif m in range(41, 61):
-                s2 = max(randint(16, m), randint(16, (m + 1)))
+                s2 = max(randint(16, m), randint(16, m))
                 results.append(s2)
             elif m in range(60, 100):
-                s3 = max(randint(23, m), randint(23, (m + 1)))
+                s3 = max(randint(23, m), randint(23, m))
                 results.append(s3)
-    elif r == "HARD":
+    elif r == "HARD" or r == "hard":
         for i in range(p):
             if m in range(20, 41):
-                f1 = min(randint(1, m), randint(1, (m + 1)))
+                f1 = min(randint(1, m), randint(1, m))
                 results.append(f1)
             elif m in range(41, 61):
-                f2 = min(randint(9, m), randint(9, (m + 1)))
+                f2 = min(randint(9, m), randint(9, m))
                 results.append(f2)
             elif m in range(60, 100):
-                f3 = min(randint(15, m), randint(15, (m + 1)))
+                f3 = min(randint(15, m), randint(15, m))
                 results.append(f3)
     else:
         pass
@@ -95,7 +95,7 @@ def analyse(array, marks):
 print("--------------------------------")
 marks = int(input("Enter the amount of marks the test will have min(20) max(100): "))
 checkMarks(marks)
-pupils = int(input("Enter the amount of pupils attending the test min(20) max(60): "))
+pupils = int(input("Enter the amount of pupils attending the test min(5) max(60): "))
 checkPupils(pupils)
 difficulty = input("Should the difficulty be EASY, MEDIUM, HARD: ")
 checkDifficulty(difficulty)
